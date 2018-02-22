1d3b1e884d3e22b2abf3c24b52c7d47774e600ec
aniruddhathorat123 <32008491+aniruddhathorat123@users.noreply.github.com>
2018-02-22 23:36:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.get("/article-one", function(req,res)
 
 app.get("/article-two",function(req,res)
 {
-   res.send("Second article call"); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get("/article-three",function(req,res)
 {
-   res.send("Third article going to send.............."); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
